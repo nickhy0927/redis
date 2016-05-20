3924d4f4b9af1069c9af879a2314ea4ef345c7ce
Administrator <Administrator@MS-20160520WPQX>
2016-05-21 02:31:47 +0800

    ..

--- src/main/java/com/redis/home/HomeController.java
+++ src/main/java/com/redis/home/HomeController.java
@@ -1,13 +1,34 @@
 package com.redis.home;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Controller
 @RequestMapping(value = "/portol")
 public class HomeController {
+	@Resource
+	SessionLocaleResolver localeResolver;
+	
+	@RequestMapping("/change")
+	public String language(HttpServletRequest request, HttpServletResponse response, String language) {
+//		language = language.toLowerCase();
+//		if (language == null || language.equals("")) {
+//			return new ModelAndView("index");
+//		} else {
+//			if (language.equals("zh_cn")) {
+//				localeResolver.setLocale(request, response, Locale.CHINA);
+//			} else if (language.equals("en_us")) {
+//				localeResolver.setLocale(request, response, Locale.ENGLISH);
+//			} else {
+//				localeResolver.setLocale(request, response, Locale.CHINA);
+//			}
+//		}
 
-	public void index() {
-		
+		return "index";
 	}
 }
